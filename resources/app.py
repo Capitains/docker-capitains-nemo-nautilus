ef45eff0e19e86559548d5d8dc476d9665b51342
ponteineptique <leponteineptique@gmail.com>
2017-07-17 08:57:37 +0200

    Upgraded to latest version of Nemo and Nautilus

--- resources/app.py
+++ resources/app.py
@@ -1,24 +1,28 @@
 # -*- coding: utf-8 -*-
 
 from flask import Flask, request
-from flask.ext.nemo import Nemo
+from flask_nemo import Nemo
 from capitains_nautilus.flask_ext import FlaskNautilus, WerkzeugCacheWrapper
+from capitains_nautilus.cts.resolver import NautilusCTSResolver
 from werkzeug.contrib.cache import FileSystemCache
-from flask_cache import Cache
-from lxml import etree
+from flask_caching import Cache
 import os
 from config import nemo_config
      
 d = "/opt/data"
 app = Flask("Nautilus")
 nautilus_cache = FileSystemCache("/opt/cache")
+http_cache = Cache(config={'CACHE_TYPE': "simple"})
+nautilus_resolver = NautilusCTSResolver(
+    [os.path.join(d,o) for o in os.listdir(d) if os.path.isdir(os.path.join(d,o))],
+    cache=nautilus_cache
+)
 nautilus = FlaskNautilus(
     app=app,
-    prefix="/api/cts",
+    prefix="/api",
     name="nautilus",
-    resources=[os.path.join(d,o) for o in os.listdir(d) if os.path.isdir(os.path.join(d,o))],
-    parser_cache=WerkzeugCacheWrapper(nautilus_cache),
-    http_cache=Cache(config={'CACHE_TYPE': "simple"})
+    resolver=nautilus_resolver,
+    flask_caching=http_cache
 )
 
 # We set up Nemo
@@ -26,14 +30,12 @@ nemo = Nemo(
     app=app,
     name="nemo",
     base_url="",
-    api_url="/api/cts",
-    retriever=nautilus.retriever,
+    cache=http_cache,
+    resolver=nautilus_resolver,
     **nemo_config
 )
-# We register its routes
-nemo.register_routes()
-# We register its filters
-nemo.register_filters()
+
+http_cache.init_app(app)
 
 app.debug = True
 if __name__ == "__main__":
